a6c003e6312feba0e8ca1bfb35ad003c969e9093
satya <38804803+satyamuralidhar@users.noreply.github.com>
2022-12-23 15:23:03 +0530

    Update vm.ps1

--- vm/vm.ps1
+++ vm/vm.ps1
@@ -1,4 +1,4 @@
 $rg = 'myrsg'
-New-AzResourceGroup -Name $rg -Location eastus -Force
+New-AzResourceGroup -Name $rg -Location eastus2 -Force
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile 'vm.json' -VnetCidr '192.168.0.0/16' -SubnetCidr '192.168.1.0/24' -Password '@myPasswd08'
 
