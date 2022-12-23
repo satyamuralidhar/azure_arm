4670152eb92d1dd5ad5592e7aedff02f3db2011d
satya <38804803+satyamuralidhar@users.noreply.github.com>
2022-12-23 15:25:08 +0530

    Update rg.ps1

--- vnet/rg.ps1
+++ vnet/rg.ps1
@@ -1,1 +1,4 @@
-New-AzResourceGroupDeployment -ResourceGroupName 'myrsg' -TemplateFile 'demo.json' -VnetCidr '192.168.0.0/16' -Subnet1Cidr '192.168.1.0/24' -Subnet2Cidr '192.168.2.0/24'
+$rg = 'myrsg'
+New-AzResourceGroup -Name $rg -Location eastus2 -Force
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile 'demo.json' -VnetCidr '192.168.0.0/16' -Subnet1Cidr '192.168.1.0/24' -Subnet2Cidr '192.168.2.0/24'
+
